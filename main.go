770e2b1fe58aa42728ba2f38f0f3784d4fcccc39
jeanduarte00 <jean_luccas320@hotmail.com>
2023-08-25 20:32:48 -0300

    feat: change style

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("Welcome - My Argo CD testing project"))
+		writer.Write([]byte("<h2 style='color: red'>Welcome - My Argo CD testing project</h2>"))
 	})
 
 	http.ListenAndServe(":8080", nil)
